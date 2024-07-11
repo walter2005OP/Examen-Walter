6825aceb7294ae78b33625afc8006f997f72fd77
walterblasquez <wa.blasquez@duocuc.cl>
2024-07-11 09:20:50 -0400

    menu

---
+++ aplicacion.py
@@ -0,0 +1,33 @@
+from funciones import *
+import os
+
+
+trabajadores = ["Juan Pérez”,”María García”,”Carlos López”,”Ana Martínez”,”Pedro Rodríguez”,”Laura Hernández”,”Miguel Sánchez”,”Isabel Gómez”,”Francisco Díaz”,”Elena Fernández"]
+
+
+while True:
+    os.system("cls")
+    print("""         ***********MENU*************
+        1. Asignar sueldos aleatorios
+        2. Clasificar sueldos
+        3. Ver estadísticas.
+        4. Reporte de sueldos
+        5. Salir del programa""")
+    
+    opc = int(input("ingrese la opcion que desea "))
+
+    if opc == 1:
+        pass
+    elif opc == 2:
+        pass
+    elif opc == 3:
+        pass
+    elif opc == 4:
+        pass
+    elif opc == 5:
+        pass
+    else:
+        pass
+        
+    
+    
